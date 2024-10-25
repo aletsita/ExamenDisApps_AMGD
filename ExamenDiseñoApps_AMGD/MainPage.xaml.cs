9faf156b3e56702229ffb317f6e86ec516219c3e
alexa <alexagastelum571@gmail.com>
2024-10-25 14:24:48 -0700

    Agregar archivos de proyecto.

---
+++ ExamenDiseñoApps_AMGD/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExamenDiseñoApps_AMGD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Suma_Clicked(object sender, EventArgs e)
+        {
+            double primerNum = Convert.ToDouble(PrimNum.Text);
+            double segundoNum = Convert.ToDouble(SecNum.Text);
+            double resultado = primerNum + segundoNum;
+
+            DisplayAlert("Resultado", $"El resultado es: {resultado}", "Ok");
+        }
+
+        private void Resta_Clicked(object sender, EventArgs e)
+        {
+            double primerNum = Convert.ToDouble(PrimNum.Text);
+            double segundoNum = Convert.ToDouble(SecNum.Text);
+            double resultado = primerNum - segundoNum;
+
+            DisplayAlert("Resultado", $"El resultado es: {resultado}", "Ok");
+        }
+
+        private void Multiplicacion_Clicked(object sender, EventArgs e)
+        {
+            double primerNum = Convert.ToDouble(PrimNum.Text);
+            double segundoNum = Convert.ToDouble(SecNum.Text);
+            double resultado = primerNum * segundoNum;
+
+            DisplayAlert("Resultado", $"El resultado es: {resultado}", "Ok");
+        }
+
+        private void Division_Clicked(object sender, EventArgs e)
+        {
+            double primerNum = Convert.ToDouble(PrimNum.Text);
+            double segundoNum = Convert.ToDouble(SecNum.Text);
+            double resultado = primerNum / segundoNum;
+
+            DisplayAlert("Resultado", $"El resultado es: {resultado}", "Ok");
+        }
+    }
+}
+
